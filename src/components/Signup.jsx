6bf77ad5369bc4801dc947b7ae49a746000e72d5
vincent anjiri <vincentanjiri12@gmail.com>
2024-10-07 10:35:47 +0300

    "Added handleSubmit function and onSubmit event handler to Signup form"

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,16 @@
 export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const acquisitionChannel = fd.getAll("acquisition");
+    const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquisitionChannel;
+    console.log(data);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
